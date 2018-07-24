9432d30ce7280346f6f88d4d295a30c1f7e7567c
zhang tian <975571814@qq.com>
2018-07-24 21:25:11 +0800

    item list complete
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/App.js
+++ src/App.js
@@ -2,22 +2,61 @@ import React, { Component } from 'react';
 import './App.css';
 import ItemList from "./ItemList";
 import CartItem from "./CartItem";
-import {loadAllItems} from './datbase';
+import {loadAllItems, loadPromotions} from './datbase';
 
+const formatItems = () => {
+  const promotions = loadPromotions();
+  return loadAllItems().map(item => {
+    const promotion = promotions.find(promotion => promotion.barcodes.includes(item.barcode));
+    let type = ''
+    if (promotion) {
+      type = promotion.type
+    }
+    return {...item, count: 0, type};
+  })
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      allItems: loadAllItems()
+      allItems: formatItems()
     }
   }
+
+  addItemCount = (item) => {
+    const {allItems} = this.state
+    const findItem = allItems.find(i => i.barcode === item.barcode);
+
+    findItem.count += 1;
+    this.setState({
+      allItems: [...allItems]
+    })
+  }
+
+  deleteItemCount = (item) => {
+    const {allItems} = this.state
+    const findItem = allItems.find(i => i.barcode === item.barcode);
+
+    if (findItem.count === 0) {
+      return
+    }
+    findItem.count -= 1;
+    this.setState({
+      allItems: [...allItems]
+    })
+  }
+
   render() {
     const {allItems} = this.state
     return (
       <div className="App">
-        <ItemList allItems={allItems}/>
+        <ItemList
+          allItems={allItems}
+          onAddItemCount={this.addItemCount}
+          onDeleteItemCount={this.deleteItemCount}
+        />
         <CartItem/>
       </div>
     );
--- src/ItemList.js
+++ src/ItemList.js
@@ -1,13 +1,46 @@
 import React, {PureComponent} from 'react';
 
 class ItemList extends PureComponent {
+
+  renderEditCount = (item) => {
+    return <div className="input-group mb-3 col-md-6 offset-md-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text" onClick={() => this.props.onDeleteItemCount(item)}>-</span>
+      </div>
+      <input type="text" className="form-control" value={item.count}/>
+      <div className="input-group-append">
+        <span className="input-group-text" onClick={() => this.props.onAddItemCount(item)}>+</span>
+      </div>
+      <span className="mt-2 ml-2">{item.unit}</span>
+    </div>
+  }
+
   render() {
     return <div>
-      {this.props.allItems.map((item, index) => {
-        return <div key={index}>
-          {item.name}
-        </div>
-      })}
+      <table className="table">
+        <thead className="thead-dark">
+        <tr>
+          <th scope="col">名称</th>
+          <th scope="col">单价</th>
+          <th scope="col">优惠信息</th>
+          <th scope="col">个数</th>
+        </tr>
+        </thead>
+        <tbody>
+        {this.props.allItems.map((item) => {
+          return <tr key={item.barcode}>
+            <td width="25%">{item.name}</td>
+            <td width="25%">{item.price}元</td>
+            <td width="25%">
+              {item.type}
+            </td>
+            <td width="25%">
+              {this.renderEditCount(item)}
+            </td>
+          </tr>
+        })}
+        </tbody>
+      </table>
     </div>
   }
 }
