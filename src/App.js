1b6e65c3d89bfc6a2f461e993b7a5677b3f1e7d1
zhang tian <975571814@qq.com>
2018-07-24 19:11:23 +0800

    init project
    Signed-off-by: zhang tian <975571814@qq.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import ItemList from "./ItemList";
+import CartItem from "./CartItem";
+import {loadAllItems} from './datbase';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allItems: loadAllItems()
+    }
+  }
   render() {
+    const {allItems} = this.state
     return (
       <div className="App">
-       app
+        <ItemList allItems={allItems}/>
+        <CartItem/>
       </div>
     );
   }
